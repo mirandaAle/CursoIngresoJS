60cc82cccec64d2453a31e5a157bc64a49e70d48
unknown <alejandro>
2020-07-14 20:40:07 +0200

    ejercicio resuelto

--- 1-EntradaSalida/jsEntradaSalida-01.js
+++ 1-EntradaSalida/jsEntradaSalida-01.js
@@ -3,7 +3,7 @@ Al presionar el  botón,
 se debe mostrar un mensaje como el siguiente "Esto funciona de maravilla"*/
 function mostrar()
 {
-	alert("Esto funciona de maravilla");
+	alert("Esto es ale");
 }
 
 ////git add .
